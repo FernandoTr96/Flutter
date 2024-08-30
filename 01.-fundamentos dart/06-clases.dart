89a61ac4aa597e2bc65fb43ce878ffac87904bee
FernandoTr96 <fernando.tr96@outlook.es>
2024-08-30 14:17:42 -0600

    override en clases

--- 01.-fundamentos dart/06-clases.dart
+++ 01.-fundamentos dart/06-clases.dart
@@ -1,5 +1,5 @@
 void main() {
-  final wolverine = Hero('Logan', 'Regeneracion');
+  final wolverine = Hero(name: 'Logan', power: 'Regeneracion');
   print(wolverine);
   print(wolverine.name);
   print(wolverine.power);
@@ -28,5 +28,20 @@ class Hero{
   */
 
   /* Una forma mas facil y comun seria */
-  Hero(this.name, this.power);
+  /* solo aplica para argumentos posicionales */
+  /* Hero(this.name, this.power); */
+
+  /* Cuando se requiere argumentos con nombre y no posicionales, requeridos y no requeridos se usa esta estrictura */
+  /* para ello los argumentos se envuelven en {} */
+  /* de esta forma ahora los argumentos requeriran nombre y puedes decidir si inicializarlos o requerirlos */
+  Hero({ required this.name, this.power = 'No power added'});
+
+  /* Override */
+  // Las clases tinen metodos heredados que suelen venir de otras de nuestras clases o de clases del lenguaje
+  // es el caso por ejemplo de toString() este metodo es heredado por todos los objetos de dart se podria decir que es un metodo por defecto
+  // al ser heredado podemos usar la notacion override para cambiar su comportamiento, este en concreto no es necesario llamarlo porque el print lo hace internamente
+  @override
+  String toString() {
+    return 'Hero{name: $name, power: $power}';
+  }
 }
