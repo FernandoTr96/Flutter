1a624fdad485a5d216289eccc2305466bbd08de5
fernando.tr96@outlook.es <fernando.tr96>
2024-09-08 14:26:53 -0600

    alertas

--- 02.-flutter/widgets_app/lib/presentation/screens/snackbar/snackbar_screen.dart
+++ 02.-flutter/widgets_app/lib/presentation/screens/snackbar/snackbar_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class SnackbarScreen extends StatelessWidget {
 
@@ -6,8 +7,63 @@ class SnackbarScreen extends StatelessWidget {
 
   const SnackbarScreen({super.key});
 
+  void showCustomSnackbar(BuildContext context){
+    ScaffoldMessenger.of(context).clearSnackBars();
+    final snackbar = SnackBar(
+      content: const Text('Hello'),
+      action: SnackBarAction(label: 'OK', onPressed: (){}),
+      duration: const Duration(seconds: 2),
+    );
+    ScaffoldMessenger.of(context).showSnackBar(snackbar);
+  }
+
+  void openDialog(BuildContext context){
+    showDialog(
+      barrierDismissible: false,
+      context: context, 
+      builder: (context) {
+        return AlertDialog(
+          title:  const Text('Are u shure ?'),
+          content: const Text('lorem ipsu dolore mis satyurn no iptetumenasdasdas'),
+          actions: [
+            TextButton(onPressed: ()=> context.pop(), child: const Text('Ok')),
+            TextButton(onPressed: ()=> context.pop(), child: const Text('Cancel')),
+          ]
+        ); 
+      }
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Alertas y dialogos')
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: () => showCustomSnackbar(context), 
+        label: const Text('Mostrar Snackbar'),
+        icon: const Icon(Icons.remove_red_eye),
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            FilledButton.tonal(
+              onPressed: (){
+                showAboutDialog(
+                  context: context,
+                  children: [const Text('Lorem ipsu dolore mia')]
+                );
+              }, 
+              child: const Text('Licencias usadas')
+            ),
+            FilledButton.tonal(
+              onPressed: () => openDialog(context), 
+              child: const Text('Mostrar dialogo')
+            )
+          ]
+        )
+      )
+    );
   }
 }
