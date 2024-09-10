ab9ce5d81f0613da16046cccb86b2df7d9608709
fernando.tr96@outlook.es <fernando.tr96>
2024-09-09 19:13:45 -0600

    drawer

--- 02.-flutter/widgets_app/lib/presentation/screens/home/home_screen.dart
+++ 02.-flutter/widgets_app/lib/presentation/screens/home/home_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:widgets_app/config/menu/menu_item.dart';
+import 'package:widgets_app/presentation/widgets/side_menu.dart';
 
 class HomeScreen extends StatelessWidget {
 
@@ -10,9 +11,12 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final scaffoldKey = GlobalKey<ScaffoldState>();
     return Scaffold(
+      key: scaffoldKey,
       appBar: AppBar(title: const Text('Widgets de Flutter')),
       body: const _HomeView(),
+      endDrawer: SideMenu(scaffoldKey: scaffoldKey),
     );
   }
 }
