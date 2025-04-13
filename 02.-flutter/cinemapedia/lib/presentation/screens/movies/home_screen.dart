80a989574f732f6e44aa8a982fda83bbe55d8404
fernando.tr96@outlook.es <fernando.tr96>
2025-04-13 11:45:46 -0600

    slivers

--- 02.-flutter/cinemapedia/lib/presentation/screens/movies/home_screen.dart
+++ 02.-flutter/cinemapedia/lib/presentation/screens/movies/home_screen.dart
@@ -4,7 +4,6 @@ import 'package:cinemapedia/presentation/widgets/index.dart';
 import 'package:cinemapedia/presentation/providers/providers.dart';
 
 class HomeScreen extends StatelessWidget {
-
   static const name = 'home_screen';
 
   const HomeScreen({super.key});
@@ -19,7 +18,6 @@ class HomeScreen extends StatelessWidget {
 }
 
 class HomeView extends ConsumerStatefulWidget {
-  
   const HomeView({
     super.key,
   });
@@ -29,28 +27,57 @@ class HomeView extends ConsumerStatefulWidget {
 }
 
 class HomeViewState extends ConsumerState<HomeView> {
-
   @override
   void initState() {
     super.initState();
     ref.read(nowPlayingMoviesProvider.notifier).loadNextPage();
   }
-  
+
   @override
   Widget build(BuildContext context) {
-
+    
     final slideShowMovies = ref.watch(moviesSlideShowProvider);
     final nowPlayingMovies = ref.watch(nowPlayingMoviesProvider);
 
-    return Column(
-      children: [
-        const CustomAppBar(),
-        MoviesSlideShow(movies: slideShowMovies),
-        MovieHorizontalListview(
-          title: 'In the movies',
-          subtitle: 'Today',
-          movies: nowPlayingMovies,
-          loadNextPage: ref.read(nowPlayingMoviesProvider.notifier).loadNextPage
+    return CustomScrollView(
+      slivers: [
+        const SliverAppBar(
+          floating: true,
+          flexibleSpace: FlexibleSpaceBar(
+            title: CustomAppBar(),
+            titlePadding: EdgeInsets.symmetric(vertical: 5),
+          )
+        ),
+        SliverList(
+          delegate: SliverChildBuilderDelegate(
+            (context, index) {
+              return Column(
+                children: [
+                  MoviesSlideShow(movies: slideShowMovies),
+                  MovieHorizontalListview(
+                    title: 'In the movies',
+                    subtitle: 'Today',
+                    movies: nowPlayingMovies,
+                    loadNextPage: ref.read(nowPlayingMoviesProvider.notifier).loadNextPage
+                  ),
+                  MovieHorizontalListview(
+                    title: 'Comming son',
+                    subtitle: 'In a moth',
+                    movies: nowPlayingMovies,
+                    loadNextPage: ref.read(nowPlayingMoviesProvider.notifier).loadNextPage
+                  ),
+                  MovieHorizontalListview(
+                    title: 'Popular',
+                    subtitle: 'This year',
+                    movies: nowPlayingMovies,
+                    loadNextPage: ref.read(nowPlayingMoviesProvider.notifier).loadNextPage
+                  ),
+                  const SizedBox(height: 15)
+                ]
+              );
+            },
+            childCount: 1
+          )
         )
       ]
     );
